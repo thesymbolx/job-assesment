e87c4436b4358d92b971c63ef9a896ea31fcd25d
dale.evans <dale.evans@ert.com>
2021-04-19 16:47:55 +0100

    Updated detail vm

--- app/src/main/java/evans/dale/job_assessment/room_details/RoomDetailsVM.kt
+++ app/src/main/java/evans/dale/job_assessment/room_details/RoomDetailsVM.kt
@@ -2,7 +2,6 @@ package evans.dale.job_assessment.room_details
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.MutableLiveData
 import dagger.hilt.android.lifecycle.HiltViewModel
 import evans.dale.job_assessment.R
 import evans.dale.job_assessment.room.RoomRepository
@@ -11,37 +10,52 @@ import javax.inject.Inject
 
 @HiltViewModel
 class RoomDetailsVM @Inject constructor(
-    private val roomRepository: RoomRepository,
-    application: Application
+        private val roomRepository: RoomRepository,
+        application: Application
 ) : AndroidViewModel(application) {
     val room: RoomDetail? = null
-    val title = MutableLiveData<String>()
-    val location = MutableLiveData<String>()
 
-    suspend fun getRoomDetails(roomKey: String) : List<DetailModel> {
+    suspend fun getRoomDetails(roomKey: String): List<DetailModel> {
         val room = roomRepository.getRoomDetails(roomKey)
 
-        return listOf(
-            DetailModel.Header(
-                getApplication<Application>().getString(R.string.details_location)
-            ),
+        return mutableListOf<DetailModel>().apply {
+            addHeader(R.string.details_location)
+            addText("""
+                |${room.location.region.name}
+                |${room.location.site.name}
+                |${room.location.building.name}
+                |${room.location.floor.name}
+            """.trimMargin())
 
-            DetailModel.Text(
-                room.location.let {
-                    """${it.region.name}
-                |${it.site.name}
-                |${it.building.name}
-                |${it.floor.name}
-            """.trimMargin()
-                }
-            )
+            addHeader(R.string.details_features)
+            room.features.forEach {
+                addText(it.name)
+            }
 
-        )
+            addHeader(R.string.details_services)
+            room.services.forEach {
+                addIconTextItem(it.name, it.iconUrl)
+            }
+
+            addHeader(R.string.details_facilities)
+            room.facilities.forEach {
+                addIconTextItem(it.name, it.iconUrl)
+            }
+        }
     }
+
+    private fun MutableList<DetailModel>.addHeader(titleId: Int) =
+            add(DetailModel.Text(getApplication<Application>().getString(titleId)))
+
+    private fun MutableList<DetailModel>.addText(value: String) =
+            add(DetailModel.Text(value))
+
+    private fun MutableList<DetailModel>.addIconTextItem(name: String, iconUrl: String) =
+            add(DetailModel.IconText(name, iconUrl))
 
     sealed class DetailModel(val layoutId: Int) {
         class Header(val title: String) : DetailModel(R.layout.details_header)
-        class Feature(val name: String) : DetailModel(R.layout.details_header)
         class Text(val value: String) : DetailModel(R.layout.details_text)
+        class IconText(val name: String, val iconUrl: String) : DetailModel(R.layout.details_icon_text)
     }
 }
